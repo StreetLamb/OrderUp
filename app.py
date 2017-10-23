049f0ad1629c1240dcd462980bb9f2c02b7dd95c
Jerron Lim <jerronlim@Jerron-Mac.local>
2017-10-23 14:32:15 +0800

    make it better

--- app.py
+++ app.py
@@ -121,6 +121,12 @@ def on_chat_message(msg):
                         count+=1
                     bot.sendMessage(i,msg)
 
+        elif msg['text']=="/logs":
+            if chat_id==243431792:
+                print(id_phonedict)
+                print(orderee_list)
+                print(all_list)
+
         elif msg['text']=="/stop":
             for i in all_list:
                 if i['userid']==str(chat_id):
@@ -362,7 +368,7 @@ def on_chat_message(msg):
             
 def on_callback_query(msg):
     query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
-    print('Callback Query:', query_id, from_id, query_data)
+##    print('Callback Query:', query_id, from_id, query_data)
     if query_data=="o1" or query_data=="o2":
 
         keyboard=InlineKeyboardMarkup(
@@ -498,7 +504,7 @@ def on_callback_query(msg):
 
 
 TOKEN1='450317924:AAFiqyqZvaCP5VgkDJ0a5kJK7wbSqo8b4Ts'
-TOKEN2='450738219:AAFa5zrteFSpNlYJOlyWfA4dzsvWQfj5-VU'
+##TOKEN2='450738219:AAFa5zrteFSpNlYJOlyWfA4dzsvWQfj5-VU'
 bot=telepot.Bot(TOKEN1)
 ##bot2=telepot.Bot(TOKEN2)
 MessageLoop(bot,{'chat': on_chat_message,'callback_query': on_callback_query, 'pre_checkout_query': on_pre_checkout_query}).run_as_thread()
